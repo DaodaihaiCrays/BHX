a6cf8e4c14e81b871ee3c29ed44ff153f03501ba
daodaihai <hai0944345963@gmail.com>
2023-03-26 16:53:19 +0700

    first commit

---
+++ app/src/main/java/com/bhx/bhx/Model/ProductModel/Product.kt
@@ -0,0 +1,3 @@
+package com.bhx.bhx.Model
+
+data class Product (val id: String, val img:String, val name:String, val quanlity:Int, val price:Double, val infor:String)
---
+++ app/src/main/java/com/bhx/bhx/Model/ProductModel/ProductAdapter.kt
@@ -0,0 +1,29 @@
+package com.bhx.bhx.Model.ProductModel
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bhx.bhx.Model.Product
+import com.bhx.bhx.R
+
+class ProductAdapter(private val productList: List<Product>) : RecyclerView.Adapter<ProductAdapter.ProductViewHolder>() {
+
+    class ProductViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val tvName: TextView = itemView.findViewById(R.id.tvName)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.product_item, parent, false)
+        return ProductViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
+        //holder.fruitNameTextView.text = fruitList[position]
+    }
+
+    override fun getItemCount(): Int {
+        return productList.size
+    }
+}
