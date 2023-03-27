15f811568ee73b3d8865dac9328cde9de549ceb4
KhoiLDM-luigi <khoiledangminh@gmail.com>
2023-03-27 22:04:40 +0700

    update account screen

--- app/src/main/java/com/bhx/bhx/View/AccountFragment/AccountFragment.kt
+++ app/src/main/java/com/bhx/bhx/View/AccountFragment/AccountFragment.kt
@@ -1,11 +1,16 @@
 package com.bhx.bhx.View.AccountFragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import com.bhx.bhx.R
+import com.bhx.bhx.View.AccountInfoActivity
+import com.bhx.bhx.View.AccountOrderActivity
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -19,15 +24,35 @@ private const val ARG_PARAM2 = "param2"
  */
 class AccountFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var accInfoBtn: Button? = null
+    private var accOrdersBtn: Button? = null
+    private var accAvatar: ImageView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+//        arguments?.let {
+//            param1 = it.getString(ARG_PARAM1)
+//            param2 = it.getString(ARG_PARAM2)
+//        }
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        accInfoBtn = getView()?.findViewById(R.id.acc_info_btn)
+        accInfoBtn?.setOnClickListener{
+            var intent = Intent(context, AccountInfoActivity::class.java)
+            startActivity(intent)
         }
+
+        accOrdersBtn = getView()?.findViewById(R.id.acc_orders_btn)
+        accOrdersBtn?.setOnClickListener{
+            var intent = Intent(context, AccountOrderActivity::class.java)
+            startActivity(intent)
+        }
+
+        accAvatar = getView()?.findViewById(R.id.acc_avatar)
+        accAvatar?.setImageResource(R.drawable.logo_login)
     }
 
     override fun onCreateView(
---
+++ app/src/main/java/com/bhx/bhx/View/AccountOrderActivity.kt
@@ -0,0 +1,22 @@
+package com.bhx.bhx.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.ActionBar
+import com.bhx.bhx.R
+
+class AccountOrderActivity : AppCompatActivity() {
+    var backBtn: Button? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account_order)
+        val actionBar: ActionBar? = supportActionBar
+        actionBar?.hide()
+
+        backBtn = findViewById(R.id.back_btn)
+        backBtn?.setOnClickListener{
+            finish()
+        }
+    }
+}
