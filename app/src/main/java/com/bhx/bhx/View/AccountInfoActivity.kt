2190422812e39c9def6f55a252809293c4801c47
KhoiLDM-luigi <khoiledangminh@gmail.com>
2023-03-31 14:29:09 +0700

    update user info

--- app/src/main/java/com/bhx/bhx/View/AccountInfoActivity.kt
+++ app/src/main/java/com/bhx/bhx/View/AccountInfoActivity.kt
@@ -1,13 +1,24 @@
 package com.bhx.bhx.View
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Spinner
+import android.widget.TextView
 import androidx.appcompat.app.ActionBar
 import com.bhx.bhx.R
+import com.bhx.bhx.View.Adapter.SpinnerAdapter
+import android.widget.ArrayAdapter as ArrayAdapter
 
 class AccountInfoActivity : AppCompatActivity() {
     var backBtn: Button? = null
+    var provinceSpinner: Spinner? = null
+    var districtSpinner: Spinner? = null
+    var wardSpinner: Spinner? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account_info)
@@ -18,5 +29,22 @@ class AccountInfoActivity : AppCompatActivity() {
         backBtn?.setOnClickListener{
             finish()
         }
+
+        provinceSpinner = findViewById(R.id.acc_province)
+        districtSpinner = findViewById(R.id.acc_district)
+        wardSpinner = findViewById(R.id.acc_ward)
+
+        var provinceList = listOf(resources.getString(R.string.address_province_description), "Hồ Chí Minh")
+        var districtList= listOf(resources.getString(R.string.address_district_description), "Hóc môn")
+        var wardList = listOf(resources.getString(R.string.address_ward_description), "Tân Hiệp")
+
+        val provinceAdapter= SpinnerAdapter(this, R.layout.custom_spinner_item, provinceList)
+        provinceSpinner?.adapter = provinceAdapter
+
+        val districtAdapter= SpinnerAdapter(this, R.layout.custom_spinner_item, districtList)
+        districtSpinner?.adapter = districtAdapter
+
+        val wardAdapter = SpinnerAdapter(this, R.layout.custom_spinner_item, wardList)
+        wardSpinner?.adapter = wardAdapter
     }
 }
