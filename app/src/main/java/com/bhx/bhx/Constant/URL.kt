6559d9f3d3bd8408b5bbe1da173df5a11357c9a1
KhoiLDM-luigi <khoiledangminh@gmail.com>
2023-04-21 08:49:42 +0700

    fixed bug from main

--- app/src/main/java/com/bhx/bhx/Constant/URL.kt
+++ app/src/main/java/com/bhx/bhx/Constant/URL.kt
@@ -3,7 +3,7 @@ package com.bhx.bhx.Constant
 class URL {
     companion object {
 //        const val DOMAIN_URL = " https://food-shop-online.onrender.com"
-//        const val DOMAIN_URL = " http://10.0.2.2:8080/" //local host
-        const val DOMAIN_URL = "https://test-server-x4ub.onrender.com"
+        const val DOMAIN_URL = " http://10.0.2.2:3001/" //local host
+//        const val DOMAIN_URL = "https://test-server-x4ub.onrender.com"
     }
 }
