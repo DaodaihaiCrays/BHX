50143e6696f2e323fb96e52d640deb817dc32db4
daodaihai <hai0944345963@gmail.com>
2023-04-30 22:51:00 +0700

    up

--- app/src/main/java/com/bhx/bhx/Constant/URL.kt
+++ app/src/main/java/com/bhx/bhx/Constant/URL.kt
@@ -2,6 +2,6 @@ package com.bhx.bhx.Constant
 
 class URL {
     companion object {
-        const val DOMAIN_URL = "http://192.168.4.107:3001"
+        const val DOMAIN_URL = "https://food-shop-online.onrender.com/"
     }
 }
